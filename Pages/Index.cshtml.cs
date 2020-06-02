7c92928bdd17d6d54a4ea55c4eec832ac721246d
Batarse1 <00014419@uca.edu.sv>
2020-06-02 12:39:09 -0600

    Git init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cambio_Climático.Controllers;
+using Cambio_Climático.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Cambio_Climático.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<Estudiante> Estudiante { get; private set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Estudiante = EstudianteController.GetEstudiante();
+        }
+    }
+}
