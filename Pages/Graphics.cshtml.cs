f099668ee0091b8f3504e89731774bc32bbf3138
Batarse1 <00014419@uca.edu.sv>
2020-07-14 16:15:49 -0600

    final-commit

--- Pages/Graphics.cshtml.cs
+++ Pages/Graphics.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Cambio_Climático.Controllers;
+using Cambio_Climático.Models;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +11,10 @@ namespace Cambio_Climático.Pages
 {
     public class GraphicsModel : PageModel
     {
+        public List<Central> centrales { get; private set; }
         public void OnGet()
         {
-
+            centrales = CentralController.CentralList();
         }
     }
 }
